b9b2e58d900e701caa216e130c7f7d1f21663bb0
FelipeArruda15 <souzafelipesouza5@gmail.com>
2020-09-30 00:01:42 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Triangle;
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Triangle x, y;
+		
+		x = new Triangle();
+		y = new Triangle();
+		
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		
+		double areaX = x.area();
+		
+		double areaY = y.area();
+		
+		System.out.printf("Triângulo X área : %.4f%n", areaX);
+		System.out.printf("Triângulo Y área : %.4f%n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Maior área: X");
+		} 
+		else {
+			System.out.println("Maior área: Y");
+		}
+		sc.close();
+	}
+
+}
